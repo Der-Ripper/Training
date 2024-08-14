4e143fbbfbadc65c2570d6894b0df7918ca8fbee
Le-1v <timaleo03@gmail.com>
2024-08-14 21:11:46 +0300

    .

--- RootMe/Programming/TCP - Encoded string/TCP - Encoded string.py
+++ RootMe/Programming/TCP - Encoded string/TCP - Encoded string.py
@@ -1,14 +1,15 @@
 import socket
+import base64
 
 HOST = "challenge01.root-me.org"
-PORT = 52002
+PORT = 52023
 BUFSIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 data = s.recv(BUFSIZE)
-result = round(int(data[171:174]) ** 0.5 * int(data[191:195]), 2)
-s.send(bytes(str(result) + "\n", "utf-8"))
+string = str(data)[str(data).index("'") + 1:str(data).rindex("'")]
+s.send(bytes(base64.b64decode(string).decode("utf-8") + "\n", "utf-8"))
 data = s.recv(BUFSIZE)
 print(data.decode("utf-8"))
 s.close()
